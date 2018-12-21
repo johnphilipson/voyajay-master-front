9af40aa525528b4aef08b0a438057279abe52700
manish <manish.sharma@saachitech.com>
2018-12-21 14:33:15 +0530

    --route name change

--- routes/index.js
+++ routes/index.js
@@ -35,42 +35,4 @@ router.get('/legal', function (req, res) {
 })
 
 
-const data = {
-  members: [
-    {
-      email_address: email,
-      status: 'subscribed',
-      merge_fields: {
-        FNAME: firstName,
-        LNAME: lastName
-      }
-    }
-  ]
-};
-
-const postData = JSON.stringify(data);
-
-const options = {
-  url: 'https://<DC>.api.mailchimp.com/3.0/lists/<YOUR_LIST_ID>',
-  method: 'POST',
-  headers: {
-    Authorization: 'auth <YOUR_API_KEY>'
-  },
-  body: postData
-};
-
-request(options, (err, response, body) => {
-  if (err) {
-    res.redirect('/fail.html');
-  } else {
-    if (response.statusCode === 200) {
-      res.redirect('/success.html');
-    } else {
-      res.redirect('/fail.html');
-    }
-  }
-});
-});
-
-
 module.exports = router;
